1019d24a02cd302f5518a46e1027e3685656e49b
applesniperz <ngoseng_gee@live.com.sg>
2024-04-16 00:34:58 +0800

    testiong mic, build works

--- rainmaker_main_project/build/esp-idf/mbedtls/mbedtls/3rdparty/p256-m/cmake_install.cmake
+++ rainmaker_main_project/build/esp-idf/mbedtls/mbedtls/3rdparty/p256-m/cmake_install.cmake
@@ -45,3 +45,7 @@ if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ FILES "/Users/senggee/esp/ESP32C6_Project/rainmaker_main_project/build/esp-idf/mbedtls/mbedtls/3rdparty/p256-m/libp256m.a")
 endif()
 
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  include("/Users/senggee/esp/ESP32C6_Project/rainmaker_main_project/build/esp-idf/mbedtls/mbedtls/3rdparty/p256-m/CMakeFiles/p256m.dir/install-cxx-module-bmi-noconfig.cmake" OPTIONAL)
+endif()
+
